411a99559adaafa166247b2bf9123df47022b180
Onkeljoe <dev@onkeljoe.de>
2022-08-17 17:25:51 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import json
+from pathlib import Path
+
+# read file list
+bribefiles = [file.name for file in Path(
+    "./oldfiles/").glob("bribe-data-*.json")]
+Path("./newfiles").mkdir(parents=True, exist_ok=True)
+
+for fileinfo in bribefiles:
+    # fileinfo = bribefiles[0]  # replace with foreach
+    # read files
+    with open(fileinfo) as json_file:
+        data = json.load(json_file)
+
+    # create new format
+    for bribe in data["bribedata"]:
+        print(bribe["poolname"])
+        if "reward" not in bribe:
+            rewardlist = []
+            for fix in bribe.get("fixedreward") or ():
+                nextval = {"type": "fixed", "token": fix["token"], "amount": fix["amount"],
+                           "isfixed": fix["isfixed"], "percentagethreshold": 0, "rewardcap": 0}
+                rewardlist.append(nextval)
+            for per in bribe.get("percentreward") or ():
+                nextval = {"type": "percent", "token": per["token"], "amount": per["amount"], "isfixed": per["isfixed"],
+                           "percentagethreshold": bribe.get("percentagethreshold") or 0, "rewardcap": bribe.get("rewardcap") or 0}
+                rewardlist.append(nextval)
+            for vote in bribe.get("pervotereward") or ():
+                print("  pervote found " + vote["token"])
+                nextval = {"type": "pervote", "token": vote["token"], "amount": vote["amount"],
+                           "isfixed": vote["isfixed"], "percentagethreshold": 0, "rewardcap": 0}
+                rewardlist.append(nextval)
+                print(nextval)
+            bribe["reward"] = rewardlist
+        else:
+            print("  reward already exists")
+
+    # write copies of files
+    with open("./newfiles/" + Path(fileinfo).name, 'w') as outfile:
+        json.dump(data, outfile, indent=2, ensure_ascii=False)
