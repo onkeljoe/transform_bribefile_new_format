9b4d05ae63efbe308868ba5f5db8fd647ab72756
Onkeljoe <dev@onkeljoe.de>
2022-09-01 18:18:59 +0200

    small correction according to documentation

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 # False: create files with old and new format included. True: create files with general "reward" only
-NEW_FORMAT_ONLY = False
+NEW_FORMAT_ONLY = True
 
 # read file list
 bribefiles = [file for file in Path("./oldfiles/").glob("bribe-data-*.json")]
@@ -23,25 +23,30 @@ for fileinfo in bribefiles:
             rewardlist = []
             for fix in bribe.get("fixedreward") or ():
                 nextval = {"type": "fixed", "token": fix["token"], "amount": fix["amount"],
-                           "isfixed": fix["isfixed"], "percentagethreshold": 0, "rewardcap": 0}
+                           "isfixed": fix["isfixed"]}
                 rewardlist.append(nextval)
             for per in bribe.get("percentreward") or ():
-                nextval = {"type": "percent", "token": per["token"], "amount": per["amount"], "isfixed": per["isfixed"],
-                           "percentagethreshold": bribe.get("percentagethreshold") or 0, "rewardcap": bribe.get("rewardcap") or 0}
+                nextval = {
+                    "type": "percent", "token": per["token"], "amount": per["amount"], "isfixed": per["isfixed"]}
                 rewardlist.append(nextval)
             for vote in bribe.get("pervotereward") or ():
                 nextval = {"type": "pervote", "token": vote["token"], "amount": vote["amount"],
-                           "isfixed": vote["isfixed"], "percentagethreshold": 0, "rewardcap": bribe.get("rewardcap") or 0}
+                           "isfixed": vote["isfixed"]}
                 rewardlist.append(nextval)
             bribe["reward"] = rewardlist
         else:
-            print("reward already exists")
+            for reward in bribe.get("reward"):
+                reward.pop("rewardcap", None)
+                reward.pop("percentagethreshold", None)
 
         # remove old keys, if no longer needed
         if NEW_FORMAT_ONLY:
             bribe.pop("fixedreward", None)
             bribe.pop("percentreward", None)
             bribe.pop("pervotereward", None)
+            for reward in bribe.get("reward") or ():
+                reward.pop("rewardcap", None)
+                reward.pop("percentagethreshold", None)
 
     # write copies of files
     filepath = "./newfiles_only_reward/" if NEW_FORMAT_ONLY else "./newfiles/"
